f1f076222fe3814c5453b88eb3bb6349651148b6
oaklen <florhwu@gmail.com>
2016-09-04 18:40:04 +0800

    wide screen css and animation done

--- light/light.js
+++ light/light.js
@@ -1,12 +1,21 @@
-// $("body").onload(
-//     function() {
-//         //fade in circle images one by one
-//         $(.circleImage)
-//     }
-// );
+$(document).ready(function(){
+    $(".imageContainer").hide(0).delay(300).fadeIn(800)
+});
 
-$(".circleImage").hover(
-    function() {
-        $(".cap").show();
-    }
-);
+$("#imgOne").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#session-one").offset().top
+    }, 900);
+});
+
+$("#imgTwo").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#session-two").offset().top
+    }, 900);
+});
+
+$("#imgThree").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#session-three").offset().top
+    }, 900);
+});
