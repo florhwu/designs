da1ac7081e9c3dfdebef2f06924dbdfcb1407ee7
oaklen <florhwu@gmail.com>
2016-09-04 16:08:49 +0800

    circular image grayscale animation done

--- light/light.js
+++ light/light.js
@@ -1,1 +1,12 @@
-function
+// $("body").onload(
+//     function() {
+//         //fade in circle images one by one
+//         $(.circleImage)
+//     }
+// );
+
+$(".circleImage").hover(
+    function() {
+        $(".cap").show();
+    }
+);
